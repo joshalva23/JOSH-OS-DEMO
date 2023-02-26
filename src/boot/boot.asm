0659b76eed7be4a2c5b9ca64e8911d45f2b47713
joshalva23 <joshuaelias.cs21@rvce.edu.in>
2023-02-26 17:02:16 +0530

    Staring from basic disc with ata controller

---
+++ src/boot/boot.asm
@@ -0,0 +1,134 @@
+ORG 0x7c00
+BITS 16
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
+_start:
+	jmp short start
+	nop
+
+ times 33 db 0
+start:
+	jmp 0:step2
+
+step2:
+	cli ; Clear interrupts
+	mov ax, 0x00
+	mov ds,ax
+	mov es,ax
+	mov ss,ax
+	mov sp, 0x7c00
+	sti ; Enables interrupts
+
+.load_protected:
+	cli
+	lgdt[gdt_descriptor]
+	mov eax, cr0
+	or eax, 0x1
+	mov cr0,eax
+	
+	jmp CODE_SEG:load32
+
+;GDT
+gdt_start:
+gdt_null:
+	dd 0x0
+	dd 0x0
+; offset 0xB
+gdt_code:       ;CS SHOULD POINT TO THIS
+	dw 0xffff; Segment limit first 0-15 bits
+	dw 0	;BASE first 0-15 bits
+	db 0	; BASE first 16-23 bits
+	db 0x9a	;Access byte
+	db 11001111b; High 4 bit flags and the low 4 bit flags
+	db 0	; Base 24-31 bits
+; offset 0x10
+gdt_data:	;DS,SS,ES, FS, GS
+	dw 0xffff	;
+	dw 0	;BASE first 0-15 bits
+	db 0	; BASE first 16-23 bits
+	db 0x92	;Access byte
+	db 11001111b; High 4 bit flags and the low 4 bit flags
+	db 0	; Base 24-31 bits 
+
+gdt_end:
+
+gdt_descriptor:
+	dw gdt_end - gdt_start - 1
+	dd gdt_start
+
+
+[BITS 32]
+load32:
+	mov eax, 1
+	mov ecx, 100
+	mov edi, 0x0100000
+	call ata_lba_read
+	jmp CODE_SEG:0x0100000
+
+ata_lba_read:
+	mov ebx,eax ; Backup the lba
+
+	;Send the highest 8 bits of the lba to hard disk controller
+	shr eax, 24
+	or eax,0xE0; selects the master drive
+	mov dx, 0x1F6
+	out dx, al
+	;Finish sending the highest 8 bits of the lba
+	
+	;Send total sectors to read
+	mov eax, ecx
+	mov dx, 0x1F2
+	out dx, al
+	;FInished sending te total sectors to read
+
+	;Send more bits of the lba
+	mov eax, ebx
+	mov dx ,0x1F3
+	out dx,al 
+	;Finisied sending more bits of the lba
+
+	;Sending more bits of the lba
+	mov dx, 0x1F4
+	mov eax, ebx; Restore the backup lba
+	shr eax, 8
+	out dx, al
+	;Finished sending more bits of the lba
+
+	;Send upper 16 bits of the LBA
+	mov dx, 0x1F5
+	mov eax,ebx
+	shr eax,16
+	out dx,al
+	;Finished sending upper 16 bits of the LBA
+
+	mov dx, 0x1F7
+	mov al,0x20
+	out dx, al
+
+	;Read sectors into memory
+.next_sector:
+	push ecx
+
+;Checking if we need to read
+.try_again:
+	mov dx, 0x1F7
+	in al, dx
+	test al,8
+	jz .try_again
+
+;We need to read 256 words at a time
+
+	mov ecx, 256
+	mov dx, 0x1F0
+	rep insw
+	pop ecx
+	loop .next_sector
+	;End of readinf sectors into memory
+	ret
+
+
+times 510-($ - $$) db 0
+dw 0xAA55
+
---
+++ src/kernel.c
@@ -0,0 +1,104 @@
+#include "kernel.h"
+#include <stdint.h>
+#include <stddef.h>
+#include "idt/idt.h"
+#include "memory/heap/kheap.h"
+#include "memory/paging/paging.h"
+#include "disk/disk.h"
+
+uint16_t* video_mem= 0;
+
+uint16_t terminal_row=0;
+uint16_t terminal_col=0;
+
+
+uint16_t terminal_make_char(char c, char color){
+    return (color <<8) | c;
+}
+
+void terminal_putchar(int x, int y, char c, char colour){
+    video_mem[ (y *VGA_WIDTH ) + x ] = terminal_make_char(c, colour);
+}
+
+void terminal_writechar(char c, char colour)
+{
+    if( c == '\n' )
+    {
+        terminal_row +=1;
+        terminal_col =0;
+        return;
+    }
+    
+    terminal_putchar(terminal_col , terminal_row, c , colour);
+    terminal_col += 1;
+
+    if( terminal_col >= VGA_WIDTH )
+    {
+        terminal_col = 0;
+        terminal_row +=1 ;
+    }
+
+}
+
+void terminal_initialize(){ //clear screen
+
+    video_mem = (uint16_t*)(0xB8000);
+    terminal_row = 0;
+    terminal_col = 0;
+
+    for(int y =0; y < VGA_HEIGHT; y++){
+
+        for(int x = 0; x < VGA_WIDTH; x++){
+
+            terminal_putchar( x , y , ' ' , 0 );
+        }
+    }
+}
+
+size_t strlen(const char* str)
+{
+    size_t len=0;
+    while(str[len]){
+        len++;
+    }
+    return len;
+}
+
+void print(const char* str)
+{
+    size_t len = strlen(str);
+    for (int i=0; i<len; i++)
+    {
+        terminal_writechar(str[i],15);
+    }
+}
+
+static struct paging_4gb_chunk* kernel_chunk = 0;
+
+void kernel_main()
+{
+    
+    terminal_initialize();
+    print("Hello World\ntest");
+    
+    kheap_init(); //Heap Initialize
+
+    idt_init(); // Initialize interrupt desciptor table
+
+    //set up paging
+    kernel_chunk = paging_new_4gb(PAGING_IS_WRITEABLE | PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL);
+
+    //switch to kernelpaging_chunk
+    paging_switch( paging_4gb_chunk_get_directory(kernel_chunk));
+
+
+    //enable paging
+    enable_paging();
+
+    char buf[512];
+    disk_read_sector(0, 1, buf);
+   
+
+    //Enable the system interrupts
+    enable_interrupts(); 
+}

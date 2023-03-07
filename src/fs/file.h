8b881ba9120d864fe5d9372068e0b6d15dfef911
joshalva23 <joshuaelias.cs21@rvce.edu.in>
2023-03-07 19:29:37 +0530

    Implementing FAT16 fseek functionality

--- src/fs/file.h
+++ src/fs/file.h
@@ -24,7 +24,6 @@ struct disk;
 typedef void*(*FS_OPEN_FUNCTION)(struct disk* disk, struct path_part* path, FILE_MODE mode);
 typedef int (*FS_READ_FUNCTION)(struct disk* disk, void* private, uint32_t size, uint32_t nmemb, char* out);
 typedef int (*FS_RESOLVE_FUNCTION)(struct disk* disk);
-
 typedef int (*FS_SEEK_FUNCTION)(void* private, uint32_t offset, FILE_SEEK_MODE seek_mode);
 
 struct filesystem
--- src/status.h
+++ src/status.h
@@ -1,12 +1,13 @@
 #ifndef STATUS_H
 #define STATUS_H
 
-#define JOSHOS_ALL_OK 0
-#define EIO 1
-#define EINVARG 2
-#define ENOMEN 3
-#define EBADPATH 4
-#define EFSNOTUS 5
-#define ERDONLY 6
+#define JOSHOS_ALL_OK   0
+#define EIO             1
+#define EINVARG         2
+#define ENOMEN          3
+#define EBADPATH        4
+#define EFSNOTUS        5
+#define ERDONLY         6
+#define EUNIMP           7
 
 #endif

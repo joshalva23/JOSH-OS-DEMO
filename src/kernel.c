291615c278d601984621a6ed18c21fce5debda8c
joshalva23 <joshuaelias.cs21@rvce.edu.in>
2023-03-08 01:53:06 +0530

    Implementing the TSS

--- src/kernel.c
+++ src/kernel.c
@@ -12,6 +12,7 @@
 #include "fs/file.h"
 #include "gdt/gdt.h"
 #include "config.h"
+#include "task/tss.h"
 
 uint16_t* video_mem= 0;
 
@@ -81,11 +82,15 @@ void panic(const char* msg)
     while(1){}
 }
 
+struct tss tss;
 struct gdt gdt_real[JOSHOS_TOTAL_GDT_SEGMENTS];
 struct gdt_structured gdt_structured[JOSHOS_TOTAL_GDT_SEGMENTS]= {
     {.base = 0x00, .limit = 0x00, .type = 0x00}, //NULL SEGMENT
-    {.base = 0x00, .limit = 0xffffffff, .type = 0x9a},
-    {.base = 0x00, .limit = 0xffffffff, .type = 0x92}
+    {.base = 0x00, .limit = 0xffffffff, .type = 0x9a},  //Kernel code segment
+    {.base = 0x00, .limit = 0xffffffff, .type = 0x92},  //Kernel data segment
+    {.base = 0x00, .limit = 0xffffffff, .type = 0xf8},   //user code segment
+    {.base = 0x00, .limit = 0xffffffff, .type = 0xf2},   //user data segment
+    {.base = (uint32_t)&tss, .limit = sizeof(tss), .type = 0xE9},    //TSS segment
 };
 
 
@@ -109,6 +114,14 @@ void kernel_main()
     disk_search_and_init();
 
     idt_init(); // Initialize interrupt desciptor table
+
+    //Setup the TSS
+    memset(&tss, 0x00, sizeof(tss));
+    tss.esp0 = 0x600000;
+    tss.ss0 = KERNEL_DATA_SELECTOR;
+
+    //Load the TSS
+    tss_load(0x28);
 
     //set up paging
     kernel_chunk = paging_new_4gb(PAGING_IS_WRITEABLE | PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL);
